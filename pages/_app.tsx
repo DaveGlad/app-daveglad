27d5092cce12988a58907e77eabfde800e125d0e
dave_glad <dave@dba.ma>
2022-07-21 23:56:35 +0100

    Adding folder css and variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//!NEXT
+import type { AppProps } from "next/app";
+
+//!REDUX
+import { store } from "@store/store";
+import { Provider } from "react-redux";
+
+//!COMPONENTS
+import Layout from "@components/layout";
+
+//!STYLES
+import "../styles/resets/reset.style.css";
+import "../styles/global/globals.style.css";
+import "../styles/colors/colors.style.css";
+import "../styles/variables/variables.style.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
